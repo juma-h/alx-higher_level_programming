462040939ea916e023cc4a6c6b879dc6beffb693
Juma-h <mitchfibi@gmail.com>
2022-06-08 20:47:14 +0300

    Matrix I

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return None
-    return list(list(map(lambda a: a*a, num_list)) for num_list in matrix)
+    new = []
+    for i in matrix:
+        new.append([j ** 2 for j in i])
+    return new
