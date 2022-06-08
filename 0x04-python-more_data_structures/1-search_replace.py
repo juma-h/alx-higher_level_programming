290ecaeb6004b236c2330f3ee703113cb4a81bdf
Juma-h <mitchfibi@gmail.com>
2022-06-08 20:12:01 +0300

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = []
-    for x in my_list:
-        if x == search:
-            new.append(replace)
-        else:
-            new.append(x)
-    return new
+    if not my_list:
+        return my_list
+    return [val if val != search else replace for val in my_list]
