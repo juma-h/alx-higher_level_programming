151adcebc9e453dd57a07a72644e225df12624af
Juma-h <mitchfibi@gmail.com>
2022-08-10 13:00:11 +0300

    table reset

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
--- commented script below
--- script that creates table if not exist with description
-CREATE TABLEIF IF NOT EXISTS first_table(
-	id INT,
-	name VARCHAR(256)
+-- script 4
+-- script that creates a table called first_table in the current database
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
 );
